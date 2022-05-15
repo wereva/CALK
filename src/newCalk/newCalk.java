e08d5b0d7e4589f0ac98e7d35920470d8b6b8f22
wereva <97wereva@gmail.com>
2022-05-15 18:35:47 +0300

    new

--- src/newCalk/newCalk.java
+++ src/newCalk/newCalk.java
@@ -3,8 +3,31 @@ package newCalk;
 import java.util.Scanner;
 
 public class newCalk {
+    public static void add(int first, int second){
+        int result = first + second;
+        System.out.println(first + "+" + second + "=" +result);
+    }
+    public static void sub(int first, int second) {
+        int result = first - second;
+        System.out.println(first + "-" + second + "=" + result);
+    }
 
-    public static void main(String[] args) {
+    public static void mul(int first, int second) {
+        int result = first * second;
+        System.out.println(first + "*" + second + "=" + result);
+    }
+
+    public static void div(int first, int second) {
+        int result = first / second;
+        if (second != 0) {
+            System.out.println(first + "/" + second + "=" + result);
+        } else {
+            System.out.println("can't divide by zero");
+        }
+    }
+
+
+            public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter first number --");
         int first = in.nextInt();
@@ -16,26 +39,21 @@ public class newCalk {
         switch (symbol) {
             case "+":
                 add(first, second);
-//                break;
-//            case "-":
-//                sub(first, second);
-//                break;
-//            case "*":
-//                mul(first, second);
-//                break;
-//            case "/":
-//                div(first, second);
+                break;
+            case "-":
+                sub(first, second);
+                break;
+            case "*":
+                mul(first, second);
+                break;
+            case "/":
+                div(first, second);
                 break;
             default:
                 System.out.println("Unknown value");
-        }
-    }
-    public static void add(int first, int second){
-        int result = first + second;
-        System.out.println(first + "+" + second + "=" +result);
-
-
+           }
+       }
     }
 
-}
+
 
